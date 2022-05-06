70dac574df109fb72bfe3a94dbcd46d82024bbe7
coderbennett <jbennett@csumb.edu>
2022-05-06 14:27:11 -0700

    add helpers to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers')
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
